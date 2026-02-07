5bfe87a013d150f8c448b80d5502798ea05ad7eb
nosensejk <antoxatur@gmail.com>
2026-02-07 18:38:36 +0300

    host change

--- db.js
+++ db.js
@@ -5,6 +5,9 @@ const pool = mysql.createPool({
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
+  waitForConnections: true,
+  connectionLimit: 10,
 });
 
 module.exports = pool;
